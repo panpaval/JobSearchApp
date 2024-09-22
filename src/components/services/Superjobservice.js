4be49f03f08e023b6279dae6c1e2717056b82a60
Paval <panpaval92@gmail.com>
2024-09-22 23:04:40 +0300

    add // to baseURL

--- src/components/services/Superjobservice.js
+++ src/components/services/Superjobservice.js
@@ -1,29 +1,9 @@
-/* 
-function handleAPICall() {}
-handleApiCall(url, params, query) {}
-
-fetchJobs() {
-const data = handleApiCall('/vacancies', {...}, {...});
-}
-
-
-function handleApiCall(url, params, query) {
-  if(params) {....}
-  if(query) {...}
-  try {
-   fetch(url) .....
-  } catch (error) {}
-  } */
-
 const appId = "2ded0931";
 const apiKey = "81aeac5d29b3f6d9c79e0f84852c2ce3";
-const baseUrl = "https:api.adzuna.com/v1/api/jobs";
+const baseUrl = "https://api.adzuna.com/v1/api/jobs";
 
 export async function requestIndustryName(params) {
-  /* const country = params?.country || "us"; */
   const country = params?.filters?.country || params?.country || "us";
-  /* console.log("NAME country111", params);
-  console.log("NAME country222", country); */
   let url = `${baseUrl}/${country}/categories?app_id=${appId}&app_key=${apiKey}`;
 
   try {
@@ -43,12 +23,6 @@ export async function requestIndustryName(params) {
     console.error("Error fetching data:", error);
   }
 }
-const initialFilters = {
-  industry: "",
-  salaryMin: "",
-  salaryMax: "",
-  country: "us",
-};
 
 export async function request(params, keyword, page) {
   if (!page) {
@@ -88,43 +62,3 @@ export async function request(params, keyword, page) {
     throw error;
   }
 }
-/* export async function request(params, keyword, page) {
-  if (!page) {
-    page = 1;
-  }
-  const country = params?.filters?.country || "us";
-
-  let url = `${baseUrl}/${country}/search/${page}?app_id=${appId}&app_key=${apiKey}&results_per_page=20`;
-
-  if (keyword) {
-    url += `&what=${keyword}`;
-  }
-
-  if (params && params.filters && params.filters.category) {
-    url += `&category=${params.filters.category}`;
-  }
-
-  if (params && params.filters && params.filters.salary) {
-    if (params.filters.salary.min > 0) {
-      url += `&salary_min=${params.filters.salary.min}`;
-    }
-    if (params.filters.salary.max > 0) {
-      url += `&salary_max=${params.filters.salary.max}`;
-    }
-  }
-
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw Error("Error!");
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw error;
-  }
-}
- */
